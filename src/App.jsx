fec0bd11b531091f1bcc1c447e4f40bd71e56b35
Sara <sarailic431@gmail.com>
2025-08-05 18:31:26 +0200

    home section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <p>Fast react pizza co.</p>
+        <form>
+          <input className="input-code" placeholder="Search order #" />
+          <button className="hidden">Submit</button>
+        </form>
+      </nav>
+
+      <section className="home-section">
+        <p className="home-intro-heading">The best pizza.</p>
+        <h1 className="home-intro-description">
+          Straight out of the oven, straight to you.
+        </h1>
+
+        <p className="welcome-text">
+          👋 Welcome! Please start by telling us your name:
+        </p>
+        <form>
+          <input className="input-name" placeholder="Your full name" />
+          <button className="hidden">Submit</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default App;
