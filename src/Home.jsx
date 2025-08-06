a73a2a092436f3ebdd296f5394abc0127972b922
Sara <sarailic431@gmail.com>
2025-08-06 18:41:40 +0200

    . . . . . . .

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setName } from "./redux/slice";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Home() {
   const [value, setValue] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const name = useSelector((state) => state.name);
 
   function submitForm(e) {
     e.preventDefault();
@@ -25,17 +27,26 @@ function Home() {
       <p className="welcome-text">
         👋 Welcome! Please start by telling us your name:
       </p>
-      <form onSubmit={submitForm} className="name-form">
-        <input
-          className="input-name"
-          placeholder="Your full name"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button className={`${value.length === 0 ? "hidden" : "order-btn"}`}>
-          Start Ordering
-        </button>
-      </form>
+
+      {name.name === "" && (
+        <form onSubmit={submitForm} className="name-form">
+          <input
+            className="input-name"
+            placeholder="Your full name"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button className={`${value.length === 0 ? "hidden" : "start-btn"}`}>
+            Start Ordering
+          </button>
+        </form>
+      )}
+
+      {name.name !== "" && (
+        <Link to="/menu">
+          <button className={`start-btn`}>Start Ordering, {name.name}</button>
+        </Link>
+      )}
     </section>
   );
 }
