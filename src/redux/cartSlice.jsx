77b31be61fa946b4a5e51d2f048b0597263978a7
Sara <sarailic431@gmail.com>
2025-08-09 13:51:52 +0200

    setting cart slice reducers

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -1,11 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = "cart";
+const initialState = [];
 
 const cartSlice = createSlice({
   name: "cartSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      state.push({
+        title: action.payload.title,
+        amount: 1,
+        price: action.payload.price,
+        id: action.payload.id,
+      });
+    },
+    increaseAmount: (state, action) => {
+      //  const index = state.findIndex((toDo) => toDo.id === action.payload.id);
+      // state[index].completed = action.payload.completed;
+      const index = state.findIndex((item) => item.id === action.payload.id);
+      state[index].amount += 1;
+    },
+    decreaseAmount: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload.id);
+      // state.cart[index].amount -= 1;
+      if (state[index].amount !== 1) {
+        state[index].amount -= 1;
+      } else state = state.filter((item) => item.id !== action.payload.id);
+    },
+    deleteCartItem: (state, action) => {
+      state = state.filter((item) => item.id !== action.payload.id);
+    },
+  },
 });
 
 export default cartSlice.reducer;
