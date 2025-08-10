08cb43cc95185d2ec46628dc7734f8f1db89b4f3
Sara <sarailic431@gmail.com>
2025-08-10 23:16:48 +0200

    . . . . ....

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import {
+  increaseAmount,
+  decreaseAmount,
+  deleteCartItem,
+} from "./redux/cartSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-function CartItem() {}
+function CartItem() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  return cart.map((item) => (
+    <article className="cart-article">
+      <p>
+        {item.amount} &times; {item.title}
+      </p>
+
+      <div className="amount-price">
+        <p>${(item.amount * item.price).toFixed(2)}</p>
+
+        <div className="amount-cont">
+          <div className="amount-btns">
+            <button
+              onClick={() => dispatch(decreaseAmount({ id: item.id }))}
+              className="decrease-btn"
+            >
+              -
+            </button>
+            <span className="amount">{item.amount}</span>
+            <button
+              onClick={() => dispatch(increaseAmount({ id: item.id }))}
+              className="increase-btn"
+            >
+              +
+            </button>
+          </div>
+
+          <button
+            onClick={() => dispatch(deleteCartItem({ id: item.id }))}
+            className="delete-btn"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </article>
+  ));
+}
 
 export default CartItem;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,10 @@
-import React from "react";
-import {
-  addToCart,
-  increaseAmount,
-  decreaseAmount,
-  deleteCartItem,
-} from "../redux/cartSlice";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import CartItem from "../CartItem";
 
 function Cart() {
   const name = useSelector((state) => state.name);
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
+
   return (
     <section className="cart-section">
       <Link to="/menu" className="menu-link">
@@ -21,45 +13,7 @@ function Cart() {
 
       <div className="cart-cont">
         <p className="cart-heading">Your cart, {name.name}</p>
-        {cart.map((item) => (
-          <article className="cart-article">
-            <p>
-              {item.amount} &times; {item.title}
-            </p>
-
-            <div className="amount-price">
-              <p>${(item.amount * item.price).toFixed(2)}</p>
-
-              <div className="amount-cont">
-                <div className="amount-btns">
-                  <button
-                    onClick={() => dispatch(decreaseAmount({ id: item.id }))}
-                    className="decrease-btn"
-                  >
-                    -
-                  </button>
-                  <span className="amount">
-                    {/* {cart.find((item) => item.id === id).amount} */}
-                    {item.amount}
-                  </span>
-                  <button
-                    onClick={() => dispatch(increaseAmount({ id: item.id }))}
-                    className="increase-btn"
-                  >
-                    +
-                  </button>
-                </div>
-
-                <button
-                  onClick={() => dispatch(deleteCartItem({ id: item.id }))}
-                  className="delete-btn"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </article>
-        ))}
+        <CartItem />
       </div>
     </section>
   );
