da503dd183ba830ea051778607dd96502a05ca7d
Sara <sarailic431@gmail.com>
2025-08-10 21:19:46 +0200

    . . . . . ......

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Cart() {}
+function Cart() {
+  return (
+    <section className="cart-section">
+      <Link to="/menu"> &larr; Back to menu</Link>
+    </section>
+  );
+}
 
 export default Cart;
