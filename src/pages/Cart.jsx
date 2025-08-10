d887b1dc59a857bb2ae77874b61c4f7fcdf63b9f
Sara <sarailic431@gmail.com>
2025-08-10 21:46:27 +0200

    ... . ... . ... . ...

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  const name = useSelector((state) => state.name);
   return (
     <section className="cart-section">
-      <Link to="/menu"> &larr; Back to menu</Link>
+      <Link to="/menu" className="menu-link">
+        &larr; Back to menu
+      </Link>
+
+      <div>
+        <p className="cart-heading">Your cart, {name.name}</p>
+      </div>
     </section>
   );
 }
