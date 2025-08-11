7c59711dfb6bcf02acc376b74b462972c0d8eaaf
Sara <sarailic431@gmail.com>
2025-08-11 22:13:23 +0200

    . . . . ......

--- src/pages/NewOrder.jsx
+++ src/pages/NewOrder.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
-function NewOrder() {}
+function NewOrder() {
+  return (
+    <section className="new-order-section">
+      <p className="cart-heading">Ready to order? Let's go!</p>
+      <form
+        style={{
+          width: "700px",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <label>
+          First Name
+          <input type="text" />
+        </label>
+        <label>
+          Phone Number
+          <input type="tel" />
+        </label>
+        <label>
+          Address
+          <input type="address" />
+        </label>
+      </form>
+    </section>
+  );
+}
 
 export default NewOrder;
